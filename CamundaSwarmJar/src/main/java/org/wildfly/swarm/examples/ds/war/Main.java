e34ef5aafb9cbdac4204d48cdef0d3c01d769e73
tobia <tobia@DESKTOP-1MPIH0P>
2016-09-27 01:33:39 +0200

    adapted camudna pizza-service example and converted to .war deployment

--- CamundaSwarmJar/src/main/java/org/wildfly/swarm/examples/ds/war/Main.java
+++ CamundaSwarmJar/src/main/java/org/wildfly/swarm/examples/ds/war/Main.java
@@ -3,67 +3,44 @@ package org.wildfly.swarm.examples.ds.war;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
 import org.wildfly.swarm.Swarm;
-import org.wildfly.swarm.datasources.DatasourcesFraction;
-import org.wildfly.swarm.jaxrs.JAXRSArchive;
+import org.wildfly.swarm.undertow.WARArchive;
 
-import de.hrw.bpm.app.LoggerDelegate;
+import de.hrw.bpm.app.ApproveOrderController;
+import de.hrw.bpm.app.OrderBusinessLogic;
+import de.hrw.bpm.app.OrderEntity;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		System.out.println("Running " + Main.class.getCanonicalName() + ".main");
 
 		// Instantiate the swarm
 		Swarm swarm = new Swarm();
 
-		// String useDB = System.getProperty("swarm.use.db", "postgres");
-		String useDB = "h2";
-		
-		// // Configure the Datasources subsystem with a driver
-		// // and a datasource
-		// switch (useDB.toLowerCase()) {
-		// case "h2":
-		// System.out.println("case h2 -> use db " + useDB);
-		// swarm.fraction(datasourceWithH2());
-		// break;
-		// default:
-		// System.out.println("case default -> use db " + useDB);
-		// swarm.fraction(datasourceWithH2());
-		// }
+		// Create deployment
+		WARArchive deployment = ShrinkWrap.create(WARArchive.class);
 
-		JAXRSArchive deployment = ShrinkWrap.create(JAXRSArchive.class);
-		deployment.addResource(MyResource.class);
+		// Add Classes
+		deployment.addClasses(ApproveOrderController.class);
+		deployment.addClasses(OrderBusinessLogic.class);
+		deployment.addClasses(OrderEntity.class);
 
-		deployment.addClasses(LoggerDelegate.class);
-
+		// Add WEB-INF Resources
 		deployment.addAsWebInfResource(new ClassLoaderAsset("META-INF/persistence.xml", Main.class.getClassLoader()),
 				"classes/META-INF/persistence.xml");
 
 		deployment.addAsWebInfResource(new ClassLoaderAsset("META-INF/processes.xml", Main.class.getClassLoader()),
 				"classes/META-INF/processes.xml");
 
-		deployment.addAsWebInfResource(new ClassLoaderAsset("process.bpmn", Main.class.getClassLoader()),
-				"classes/process.bpmn");
+		deployment.addAsWebInfResource(new ClassLoaderAsset("pizza-order.bpmn", Main.class.getClassLoader()),
+				"classes/pizza-order.bpmn");
 
+		// Add dependencies defined in pom.xml
 		deployment.addAllDependencies();
 
 		// Start swarm
 		swarm.start();
 		swarm.deploy(deployment);
 
-	}
-
-	private static DatasourcesFraction datasourceWithH2() {
-		return new DatasourcesFraction().jdbcDriver("h2", (d) -> {
-			d.driverClassName("org.h2.Driver");
-			d.xaDatasourceClass("org.h2.jdbcx.JdbcDataSource");
-			d.driverModuleName("com.h2database.h2");
-		}).dataSource("ExampleDS", (ds) -> {
-			ds.driverName("h2");
-			ds.connectionUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
-			ds.userName("sa");
-			ds.password("sa");
-		});
 	}
 
 }
